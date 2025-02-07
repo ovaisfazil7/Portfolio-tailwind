ccf4a2f480701c1eeb71e0f651da10d619e7dac9
ovaisfazil7 <fazilovais0@gmail.com>
2025-02-07 23:52:01 +0500

    Add files via upload

---
+++ Portfolio-main/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+      <footer className="bg-[#0a0908] text-[#a9927d] py-6 text-center text-md lg:text-2xl font-[Caveat]">© 2024 Taha Ahmed. All rights reserved.</footer>
+    </>
+  )
+}
+
+export default Footer
